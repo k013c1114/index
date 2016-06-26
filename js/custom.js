0cd3ceab819d91545551bb0329c5e171b00bf474
さわき <kaname3236@gmail.com>
2016-06-26 19:26:21 +0900

    2016/06/26エラー対策

--- js/custom.js
+++ js/custom.js
@@ -17,10 +17,11 @@ $(function(){
 });
 
 $(function(){
+	"use strict";
 	$('a.menu-list').click(function(){
 		var speed = 700;
 		var href= $(this).attr("href");
-		var target = $(href == "#" || href == "" ? 'html' : href);
+		var target = $(href === "#" || href === "" ? 'html' : href);
 		var position = target.offset().top;
 		$("html, body").animate({scrollTop:position}, speed, "swing");
 		return false;
@@ -29,6 +30,7 @@ $(function(){
 
 
 $(function() {
+	"use strict";
   $("#scroll-top").click(function() {
     $('body, html').animate({scrollTop: 0}, 1100, 'linear');
   });
@@ -41,13 +43,13 @@ $(function() {
     var windowHeight = $(window).height();
 
     if (scrollTop >= windowHeight
-        || ($(document).height() - windowHeight) <= scrollTop) {
+         ($(document).height() - windowHeight) <= scrollTop) {
       // ウィンドウの縦幅以上にスクロールしていた、またはページの下端に達していた場合、ボタンを表示する
       $toTopButton.show();
     } else {
       $toTopButton.hide();
     }
-  }
+  };
   // ウィンドウをスクロール・ロード・リサイズしたときを契機に、ボタンの表示・非表示を変更する
   $(window).scroll(changeButtonState)
            .load(changeButtonState)
